c99fa077978fc261dd62f4b79fad0e9143ed2341
Jakub Augustynowicz <j.augustynowicz@multiplay.pl>
2017-12-15 15:27:55 +0100

    Added support for Collection and Laravel 5.4

---
+++ src/Builders/BaseBuilder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jaugustynowicz
+ * Date: 15.12.17
+ * Time: 10:18
+ */
+
+namespace Vuetable\Builders;
+
+
+class BaseBuilder
+{
+
+}
---
+++ src/Builders/CollectionVuetableBuilder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jaugustynowicz
+ * Date: 15.12.17
+ * Time: 10:09
+ */
+
+namespace Vuetable\Builders;
+
+
+class CollectionVuetableBuilder
+{
+
+}
